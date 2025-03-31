26124360239f439c96889b21b2b0b272d7f6970f
Layth Ghnaim <148519027+bnaner@users.noreply.github.com>
2025-03-31 22:48:32 +0000

    Removed unecessary comment

--- src/Home.jsx
+++ src/Home.jsx
@@ -15,32 +15,4 @@ const Home = () => {
   );
 };
 
-/*const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '100vh',
-    backgroundColor: 'white',
-    fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif',
-  },
-  logo: {
-    fontSize: '24px',
-    fontWeight: 'bold',
-    color: '#FFD700', // Gold color for the logo
-  },
-  content: {
-    flex: 1,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: '20px',
-  },
-  heading: {
-    fontSize: '32px',
-    color: '#333',
-    textAlign: 'center',
-  }
-};
-*/
-
 export default Home;
